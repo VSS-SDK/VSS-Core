b27382a99cf56b877955235fbe9b0c53dbc14deb
johnathan <johnathanfercher22@gmail.com>
2016-11-14 12:24:55 -0200

    added interface

--- rust/mod.rs
+++ rust/mod.rs
@@ -6,6 +6,45 @@
  * file, You can obtain one at http://www.gnu.org/licenses/gpl-3.0/.
  */
 
- mod {
+extern crate zmq;
+extern crate protobuf;
+
+pub mod state;
+pub mod command;
+pub mod debug;
+
+use std::str;
+
+use zmq::{Context, SUB, PAIR};
+use protobuf::{parse_from_bytes, RepeatedField, Message};
+
+use state::Global_State;
+use command::{Global_Commands, Robot_Command};
+use debug::Global_Debug;
+
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Interface {
+    context_recv: Context,
+    context_send: Context,
+    global_state: Global_State,
+    global_commands: Global_Commands,
+    global_debug: Global_Debug,
+}
+
+impl Interface {
+    pub fn new() -> Interface {
+        Interface {
+            context_recv: Context::new(),
+            context_send: Context::new(),
+            global_state: Global_State:new(),
+            global_commands: Global_Commands:new(),
+            global_debug: Global_Debug:new(),
+        };
+    }
+}
+
+mod {
      
- }
+}
