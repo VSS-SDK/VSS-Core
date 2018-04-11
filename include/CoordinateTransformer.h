c83beeb8cb42437ce6c8424826a6ec737caef270
Alexandre <alexandremstf@gmail.com>
2018-04-11 19:18:57 -0300

    Consideração pull request

---
+++ include/CoordinateTransformer.h
@@ -0,0 +1,14 @@
+#ifndef _COORDINATE_TRANSFORMER_H_
+#define _COORDINATE_TRANSFORMER_H_
+
+#include "State.h"
+
+class CoordinateTransformer {
+
+public:
+    Ball spin180Degrees(Ball ball);
+    Robot spin180Degrees(Robot robot);
+    State spinField180Degrees(State state);
+};
+ 
+#endif
