adcab6dea2edab4139db1df82a0f72f2f9a504c9
johnathan <johnathanfercher22@gmail.com>
2018-06-17 14:57:46 -0300

    iniciado testes de commandMapper e adicionado protobuf ao binario de testes

---
+++ test/Helpers/CommandMapperTests.cpp
@@ -0,0 +1,30 @@
+//
+// Created by johnathan on 17/06/18.
+//
+
+#include <googletest/googletest/include/gtest/gtest.h>
+#include <Helpers/CommandMapper.h>
+#include <Helpers/DomainRandomizer.h>
+
+TEST(setupWheelCommand, ShouldSetRightValues){
+    auto robotCommand = vss_command::Robot_Command();
+    auto wheelsCommand = vss::DomainRandomizer::createRandomWheelsCommand();
+
+    vss::CommandMapper::setupWheelCommand(&robotCommand, wheelsCommand);
+
+    EXPECT_EQ(wheelsCommand.leftVel, robotCommand.left_vel());
+    EXPECT_EQ(wheelsCommand.rightVel, robotCommand.right_vel());
+}
+
+TEST(commandToGlobalCommands, ShouldSetRightValues){
+    auto command = vss::DomainRandomizer::createRandomCommand();
+
+    auto globalCommands = vss::CommandMapper::commandToGlobalCommands(command);
+
+    EXPECT_EQ(command.commands.size(), (unsigned)globalCommands.robot_commands_size());
+
+    for(unsigned int i = 0 ; i < command.commands.size() ; i++) {
+        EXPECT_EQ(command.commands[i].leftVel, globalCommands.robot_commands(i).left_vel());
+        EXPECT_EQ(command.commands[i].rightVel, globalCommands.robot_commands(i).right_vel());
+    }
+}
