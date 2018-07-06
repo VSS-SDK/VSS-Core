0346e9f95773218a09acaf5f91d6355f7c044989
johnathan <johnathanfercher22@gmail.com>
2018-07-06 14:36:23 -0300

    corrigido versao da boost

--- scripts/install-debian9.sh
+++ scripts/install-debian9.sh
@@ -2,6 +2,6 @@
 
 INSTALL_DEBIAN_9 () {
     apt-get -y install pkgconf
-    apt-get -y install g++ cmake libzmq5 libzmq3-dev protobuf-compiler libprotobuf-dev libboost-dev
+    apt-get -y install g++ cmake libzmq5 libzmq3-dev protobuf-compiler libprotobuf-dev libboost-all-dev
     INSTALLED=1
 }
--- scripts/install-ubuntu16-10.sh
+++ scripts/install-ubuntu16-10.sh
@@ -2,6 +2,6 @@
 
 INSTALL_UBUNTU_16_10 () {
     apt-get -y install pkgconf
-    apt-get -y install g++ cmake libzmq5 libzmq3-dev protobuf-compiler libprotobuf-dev libboost-dev
+    apt-get -y install g++ cmake libzmq5 libzmq3-dev protobuf-compiler libprotobuf-dev libboost-all-dev
     INSTALLED=1
 }
